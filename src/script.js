0c1aa5d4fb0ba2011efd267a0d800f4f8af23bf8
4samsamAC <58746460+4samsamAC@users.noreply.github.com>
2026-05-25 23:26:01 +0200

    add difficulty levels for the game

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 const suits = ["♠", "♥", "♦", "♣"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+const difficulties = { // ill see for a custome one
+    easy: 8,
+    medium: 16,
+    hard: 24
+  };
+
 function createDeck() { // maybe ill do an class for the deck and cards later for now its make it
   const deck = [];
 
