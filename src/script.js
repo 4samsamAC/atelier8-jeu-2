7eb2bc1134c55ec27b5723ab1d1c37757603458b
4samsamAC <58746460+4samsamAC@users.noreply.github.com>
2026-05-25 23:33:45 +0200

    Refactor script.js for my poor eyes

--- src/script.js
+++ src/script.js
@@ -5,20 +5,20 @@ const difficulties = { // ill see for a custome one
     easy: 8,
     medium: 16,
     hard: 24
-  };
+};
 
 function createDeck() { // maybe ill do an class for the deck and cards later for now its make it
-  const deck = [];
+    const deck = [];
 
-  for (const suit of suits) {
-    for (const value of values) {
-      deck.push({
-        symbol: `${value}${suit}`
-      });
+    for (const suit of suits) {
+        for (const value of values) {
+            deck.push({
+                symbol: `${value}${suit}`
+            });
+        }
     }
-  }
 
-  return deck;
+    return deck;
 }
 
 function shuffle(array) {
@@ -26,23 +26,23 @@ function shuffle(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-  }
+}
 
 function createGame(cardCount = 8) { // againe ill maybe do a class of it
     const deck = createDeck();
-  
+
     const pairCount = cardCount / 2;
-  
+
     const shuffledDeck = shuffle(deck);
-  
+
     const selectedCards = shuffledDeck.slice(0, pairCount);
-  
+
     const gameCards = [...selectedCards, ...selectedCards];
-  
+
     return shuffle(gameCards).map((card, index) => ({
-      id: index,
-      symbol: card.symbol,
-      flipped: false,
-      matched: false
+        id: index,
+        symbol: card.symbol,
+        flipped: false,
+        matched: false
     }));
-  }
+}
