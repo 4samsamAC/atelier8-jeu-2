14071a9b5476ae7d5a4c6bb88f8f99b2349d46e2
4samsamAC <58746460+4samsamAC@users.noreply.github.com>
2026-05-29 08:24:07 +0200

    Split script.js into multiple for file for better readability

---
+++ src/js/cards.js
@@ -0,0 +1,45 @@
+const suits = ["♠", "♥", "♦", "♣"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+const difficulties = { // ill see for a custome one
+    easy: 8,
+    medium: 16,
+    hard: 24
+};
+
+let firstCard = null;
+let secondCard = null;
+
+let lockBoard = false;
+
+function createDeck() { // maybe ill do an class for the deck and cards later for now its make it
+    const deck = [];
+
+    for (const suit of suits) {
+        for (const value of values) {
+            deck.push({
+                symbol: `${value}${suit}`
+            });
+        }
+    }
+
+    return deck;
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function startGame(difficulty) {
+    const cardCount = difficulties[difficulty];
+  
+    cards = createGame(cardCount);
+  
+    resetTurn();
+  
+    updateUI();
+}
